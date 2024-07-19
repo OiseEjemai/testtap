91da5590e675769a154fb3c8d697a02ee7570947
CryptoMorki3s <cryptomorki3s@gmail.com>
2024-07-19 21:00:50 +0100

    first commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Earn from './Earn';
-import Boost from './Boost';
 import Test from './TestTap';
 
 const App = () => {
   return(
     <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/earn" element={<Earn />} />
-          <Route path="/boost" element={<Boost />} />
-          <Route path="/test" element={<Test />} />
+          <Route path="/" element={<Test />} />
         </Routes>
     </Router>
   )
